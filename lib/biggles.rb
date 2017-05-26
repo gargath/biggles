f258f80f0ddf5d53e91412b450ae48e11cb489fb
gargath <signup@lightweaver.info>
2017-05-26 14:32:19 +0100

    Add rubocop

--- lib/biggles.rb
+++ lib/biggles.rb
@@ -1,9 +1,8 @@
-require "biggles/version"
+require 'biggles/version'
 
+# Biggles is a simple job scheduler and manager based on ActiveRecord
 module Biggles
-
   def self.hello(name)
     puts "hello #{name}"
   end
-
 end
