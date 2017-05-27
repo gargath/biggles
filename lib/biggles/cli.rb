168ddfc4baf2e5ab73ba5b7bc5af5b198b793785
gargath <signup@lightweaver.info>
2017-05-27 22:58:49 +0100

    Add DB connection error handling

--- lib/biggles/cli.rb
+++ lib/biggles/cli.rb
@@ -1,12 +1,18 @@
 require 'yaml'
 require 'logger'
+require 'otr-activerecord'
 
 module Biggles
   # Module for controlling command-line tool behaviour
   module CLI
     def self.parse_config(filename)
       options = { 'loglevel' => 'INFO' }
-      options.merge! YAML.load_file(filename)
+      begin
+        options.merge! YAML.load_file(filename)
+      rescue => e
+        STDERR.puts "Failed to parse configuration file #{filename}: #{e.message}"
+        exit 1
+      end
     end
 
     def self.traps
@@ -24,14 +30,38 @@ module Biggles
     end
 
     def self.start
-      traps
       opts = parse_config('config.yaml')
+      connect(opts)
+      traps
       @logger = Logger.new(STDOUT)
       @logger.level = opts['loglevel']
       @logger.info 'Biggles starting...'
       @logger.debug 'Debug logging enabled'
       Kernel.loop do
         sleep 1
+      end
+    end
+
+    def self.connect(opts)
+      private_class_method
+      begin
+        if opts.key? 'database'
+          puts 'Using DB configuration from Biggles config file'
+          OTR::ActiveRecord.configure_from_hash! opts['database']
+        elsif File.exist?("#{Dir.pwd}/config/database.yaml")
+          puts "No DB configuration found. Using default #{Dir.pwd}/config/database.yaml"
+          OTR::ActiveRecord.configure_from_file!(
+            "#{Dir.pwd}/config/database/yaml"
+          )
+        else
+          STDERR.puts 'No DB configuration found. Biggles cannot continue.'
+          exit 2
+        end
+        ActiveRecord::Base.connection
+      rescue => e
+        STDERR.puts 'Failed to configure DB connection:'
+        STDERR.puts e.message
+        exit 2
       end
     end
   end
